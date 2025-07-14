d3b983e7dc612942597808004dad6f253f3f0bc2
hsrokon <hsrokonofficial@gmail.com>
2025-07-14 14:23:36 +0600

    image added with proper design in contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,27 +5,38 @@ const Contact = () => {
     return (
         <div className='min-h-screen'>
 
+            <section className="w-full py-16 px-6 bg-base-200">
+                <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-12 items-center">
+                    
+                    <div className="space-y-6 self-center">
+                    <h2 className="text-3xl md:text-4xl font-bold text-primary">Get in touch</h2>
+                    <p className="text-base-content">
+                        We'd love to hear about your project or questions.
+                    </p>
+                    <div className="space-y-2">
+                        <p><strong>Email:</strong> hello@yourbrand.com</p>
+                        <p><strong>Phone:</strong> +123 456 789</p>
+                        <p><strong>Location:</strong> 123 Business St, City, Country</p>
+                        <p className='italic font-thin text-sm'>
+                        <span className='text-red-600'>Things you should know:</span>
+                        &nbsp;We don't spam & we reply within 24 hours!
+                        </p>
+                    </div>
+                    </div>
+
+                    <div>
+                        <img
+                            className="w-full rounded-md"
+                            src="https://i.ibb.co/DgDWQmtq/edited-contact-reduced.png"
+                            alt="Contact"
+                        />
+                        </div>
+
+                    </div>
+            </section>
 
             <section>
                 <MessageSection></MessageSection>
-            </section>
-            
-            <section className="max-w-3xl mx-auto text-center py-16 px-6 bg-base-200"> 
-                <div className="space-y-6">
-                    <h2 className="text-3xl md:text-4xl font-bold text-primary">Get in touch</h2>
-                    <p className="text-base-content">
-                    We'd love to hear about your project or questions.
-                    </p>
-                    <div className="space-y-2">
-                    <p><strong>Email:</strong> hello@yourbrand.com</p>
-                    <p><strong>Phone:</strong> +123 456 789</p>
-                    <p><strong>Location:</strong> 123 Business St, City, Country</p>
-                    <p className='italic font-thin'>
-                        <span className='text-red-600'>Things you should know: </span>
-                     We don't spam & we reply within 24 hours!
-                    </p>
-                    </div>
-                </div>
             </section>
         </div>
     );
